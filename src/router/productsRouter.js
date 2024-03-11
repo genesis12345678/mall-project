720fa1762126f66bad5f01b68c0d1178fd87890d
장근영 <jky00914@naver.com>
2024-03-11 20:26:28 +0900

    상품 등록 컴포넌트 처리

---
+++ src/router/productsRouter.js
@@ -0,0 +1,33 @@
+import { Suspense, lazy } from "react";
+import { Navigate } from "react-router-dom";
+
+const Loading = <div className={"bg-red-600"}>Loading....</div>;
+const PrdouctList = lazy(() => import("../pages/products/ListPage"));
+const PrdouctAdd = lazy(() => import("../pages/products/AddPage"));
+
+const productsRouter = () => {
+  return [
+    {
+      path: "list",
+      element: (
+        <Suspense fallback={Loading}>
+          <PrdouctList />
+        </Suspense>
+      ),
+    },
+    {
+      path: "",
+      element: <Navigate replace to="/products/list" />,
+    },
+    {
+      path: "add",
+      element: (
+        <Suspense fallback={Loading}>
+          <PrdouctAdd />
+        </Suspense>
+      ),
+    },
+  ];
+};
+
+export default productsRouter;
--- src/router/root.js
+++ src/router/root.js
@@ -1,5 +1,7 @@
 import { Suspense, lazy } from "react";
 import todoRouter from "./todoRouter";
+import productsRouter from "./productsRouter";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const Loading = <div className={"bg-red-600"}>Loading....</div>;
@@ -7,6 +9,7 @@ const Loading = <div className={"bg-red-600"}>Loading....</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
+const ProudctsIndex = lazy(() => import("../pages/products/IndexPage"));
 
 const root = createBrowserRouter([
   {
@@ -33,6 +36,15 @@ const root = createBrowserRouter([
       </Suspense>
     ),
     children: todoRouter(),
+  },
+  {
+    path: "products",
+    element: (
+      <Suspense fallback={Loading}>
+        <ProudctsIndex />
+      </Suspense>
+    ),
+    children: productsRouter(),
   },
 ]);
 
