90b029ed91e875900ecfe77799c7b18fa251bd24
장근영 <jky00914@naver.com>
2024-03-18 15:20:26 +0900

    로그인/로그아웃 처리

--- src/router/root.js
+++ src/router/root.js
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from "react";
 import todoRouter from "./todoRouter";
 import productsRouter from "./productsRouter";
+import memberRouter from "./memberRouter";
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -45,6 +46,10 @@ const root = createBrowserRouter([
       </Suspense>
     ),
     children: productsRouter(),
+  },
+  {
+    path: "member",
+    children: memberRouter(),
   },
 ]);
 
---
+++ src/slices/loginSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initState = {
+  email: "",
+};
+
+const loginSlice = createSlice({
+  name: "loginSlice",
+  initialState: initState,
+  reducers: {
+    login: (state, action) => {
+      console.log("login.........", action);
+      console.log(action.payload);
+      console.log("----------------------");
+      return { email: action.payload.email };
+    },
+    logout: () => {
+      console.log("logout.........");
+
+      return { ...initState };
+    },
+  },
+});
+
+export const { login, logout } = loginSlice.actions;
+
+export default loginSlice.reducer;
