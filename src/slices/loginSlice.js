e36811532e8014c50af1a35629c936d73c7c69eb
장근영 <jky00914@naver.com>
2024-03-18 18:22:11 +0900

    AccessToken & RefreshToken

--- src/slices/loginSlice.js
+++ src/slices/loginSlice.js
@@ -1,12 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { loginPost } from "../api/memberApi";
+import { getCookie, removeCookie, setCookie } from "../util/cookieUtil";
 
 const initState = {
   email: "",
 };
 
+const loadMemberCookie = () => {
+  const memberInfo = getCookie("member");
+  if (memberInfo && memberInfo.nickname) {
+    memberInfo.nickname = decodeURIComponent(memberInfo.nickname);
+  }
+
+  return memberInfo;
+};
+
+export const loginPostAsync = createAsyncThunk("loginPostAsync", (param) => {
+  return loginPost(param);
+});
+
 const loginSlice = createSlice({
   name: "loginSlice",
-  initialState: initState,
+  initialState: loadMemberCookie() || initState,
   reducers: {
     login: (state, action) => {
       console.log("login.........", action);
@@ -17,8 +32,30 @@ const loginSlice = createSlice({
     logout: () => {
       console.log("logout.........");
 
+      removeCookie("member");
+
       return { ...initState };
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginPostAsync.fulfilled, (state, action) => {
+        console.log("fulfilled");
+
+        const payload = action.payload;
+
+        if (!payload.error) {
+          setCookie("member", JSON.stringify(payload), 1);
+        }
+
+        return payload;
+      })
+      .addCase(loginPostAsync.pending, (state, action) => {
+        console.log("pending");
+      })
+      .addCase(loginPostAsync.rejected, (state, action) => {
+        console.log("rejected");
+      });
   },
 });
 
